527a3a24919d30df18384a49a76c08c94686d06c
ncc <kwilkerson723@gmail.com>
2017-03-29 10:43:31 -0700

    make shared on add addventurer is giving an error

--- Project 3 Starting Point/Project 3 Starting Point/Adventurer.h
+++ Project 3 Starting Point/Project 3 Starting Point/Adventurer.h
@@ -1,8 +1,21 @@
 #pragma once
+#include <string>
 class Adventurer
 {
+private:
+	std::string _name;
+
 public:
-	Adventurer();
+	
+	
+	Adventurer(std::string name);
 	~Adventurer();
+
+	std::string getName() const { return _name };
+	std::string virtual attack(); {return _name + "Punches with meaty fists!";}
+
+	//virtual means that derived classes can over ride it
+
+
 };
 
